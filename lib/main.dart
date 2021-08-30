208ab699c2b99065f0fe82ebe55fa7788b296e4f
tamzidpeace <tamjedpeace@gmail.com>
2021-08-30 14:16:28 +0600

    poc done

--- lib/main.dart
+++ lib/main.dart
@@ -41,10 +41,50 @@ class _MyHomePageState extends State<MyHomePage> {
         autofocus: true,
         onKey: (RawKeyEvent event) {
           if (event is RawKeyDownEvent) {
-            print(event.data.logicalKey.keyLabel);
-          } else if (event is RawKeyUpEvent) {
-            // handle key up
-          }
+            var key = event.data.logicalKey.keyLabel;
+            if (key == "Enter") {
+              int code = int.parse(result);
+              bool isValid = false;
+              for (var i = 0; i <= 10; i++) {
+                if (i == code) {
+                  isValid = true;
+                  break;
+                }
+              }
+              print(isValid);
+
+              if (isValid) {
+                setState(() {
+                  if (!qrcode[code]['isScanned']) {
+                    message = "Valid QR Code!";
+                    qrcode[code]['isScanned'] = true;
+                  } else {
+                    message = "Already Scanned! Test different one.";
+                  }
+                });
+              } else {
+                setState(() {
+                  message = "Invalid QR Code!";
+                });
+              }
+              result = "";
+            } else {
+              String x = key.toString();
+              if (x == "0" ||
+                  x == "1" ||
+                  x == "2" ||
+                  x == "3" ||
+                  x == "4" ||
+                  x == "5" ||
+                  x == "6" ||
+                  x == "7" ||
+                  x == "7" ||
+                  x == "8" ||
+                  x == "9") {
+                result += key.toString();
+              }
+            }
+          } else if (event is RawKeyUpEvent) {}
         },
         child: Container(
           width: double.infinity,
@@ -66,7 +106,7 @@ class _MyHomePageState extends State<MyHomePage> {
                     ),
                     child: Center(
                         child: Text(
-                      "Scan QR Code",
+                      "$message",
                       style: TextStyle(fontSize: 30),
                     )),
                   ),
@@ -89,7 +129,7 @@ class _MyHomePageState extends State<MyHomePage> {
                       padding: const EdgeInsets.symmetric(
                           horizontal: 10, vertical: 30),
                       child: ListView.builder(
-                        itemCount: 10,
+                        itemCount: qrcode.length,
                         itemBuilder: (BuildContext context, int index) {
                           return ListTile(
                             title: Container(
@@ -102,7 +142,9 @@ class _MyHomePageState extends State<MyHomePage> {
                                       BorderRadius.all(Radius.circular(20))),
                               child: Center(
                                 child: Text(
-                                  '00000${index + 1}',
+                                  qrcode[index]['isScanned']
+                                      ? "Scanned"
+                                      : '${qrcode[index]['code']}',
                                   style: TextStyle(fontSize: 30),
                                 ),
                               ),
@@ -120,4 +162,27 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
     );
   }
+
+  //* functionalities
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  List qrcode = [
+    {"code": 0, "isScanned": false},
+    {"code": 1, "isScanned": false},
+    {"code": 2, "isScanned": false},
+    {"code": 3, "isScanned": false},
+    {"code": 4, "isScanned": false},
+    {"code": 5, "isScanned": false},
+    {"code": 6, "isScanned": false},
+    {"code": 7, "isScanned": false},
+    {"code": 8, "isScanned": false},
+    {"code": 9, "isScanned": false},
+    {"code": 10, "isScanned": false},
+  ];
+
+  String result = "";
+  String message = "Scan QR Code";
 }
